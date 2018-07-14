b66b0cb791534332098a2b3d7e9b6445cbd0ceab
Tabitha Goodall <tg.twitchett@gmail.com>
2018-07-14 14:04:39 +0200

    chore: initial commit
    bare create-react-native app

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, FlatList, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
